81bd36206c682cdb9fca9769f79180eec5391730
houndsito <fcampoverdeg@gmail.com>
2025-05-27 01:35:53 -0400

    created basic frontend, and set up https connection to deploy website

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a brief introduction about myself and my work.</p>
+      <p>Feel free to explore my projects and get in touch!</p>
+    </div>
+  );
+};
+
+export default Home;
