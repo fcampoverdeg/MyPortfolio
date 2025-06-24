04f550c15e219cdab5a7793b8c48a59ba35762d7
houndsito <fcampoverdeg@gmail.com>
2025-06-24 13:37:53 -0400

    Fixed warnings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TypewriterEffect from "./TypewriterEffect";
-import coverImage from "../images/cover.jpg";
+
 import "./Home.css";
 
 const Home = () => {
